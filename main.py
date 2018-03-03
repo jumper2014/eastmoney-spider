b4bba375281f42ee52e86a8a1997752e4dd1796d
zyt <ijumper@163.com>
2018-03-03 16:51:55 +0800

    fix stock number stats bug

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding=utf-8
-
-# 获取所有股票的代码和名称
+# author: zengyuetian
+# 从东方财富网上获取所有股票的代码和名称
 
 import urllib2
 import re
@@ -9,10 +9,12 @@ import time
 from bs4 import BeautifulSoup
 
 import sys
-reload(sys)  # Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
+# Python2.5 初始化后会删除 sys.setdefaultencoding 这个方法，我们需要重新载入
+reload(sys)
 sys.setdefaultencoding('utf-8')
 
-start = time.time()
+start = time.time()     # 启动计时
+
 # 爬取页面内容
 response = urllib2.urlopen('http://quote.eastmoney.com/stocklist.html')
 html = response.read(response)
@@ -26,7 +28,6 @@ li_elements = soup.find_all("li")
 # ()中间有代码的项，()需要转义
 pattern1 = ".*\(\d+\).*"
 pattern2 = ".*>(.*)</a.*"   # 尝试匹配  基金裕阳(500006)
-count = 0   # 股票支数
 stocks = {}
 
 for item in li_elements:
@@ -34,7 +35,6 @@ for item in li_elements:
 
     if re.match(pattern1, item_string):
         m = re.match(pattern2, item_string)
-        count += 1
         name_code = m.group(1)
         idx1 = name_code.index("(")
         idx2 = name_code.index(")")
@@ -48,9 +48,11 @@ sorted_stocks = sorted(stocks.iteritems(), key=lambda d: int(d[1]), reverse=Fals
 for name, code in sorted_stocks:
     print(name + "->" + code)
     lines.append(name + "," + code+'\n')
-print("Total stock num: {0}".format(count))
+print("Total stock num: {0}".format(len(sorted_stocks)))
 
-
+# 写入csv文件，为了速度，直接通过文件形式写入，如果使用csv库，性能会大幅下降
 with open('data.csv', 'w') as f:
     f.writelines(lines)
+
+# 打印总的消耗时间
 print("Time: {0}".format(time.time() - start))
